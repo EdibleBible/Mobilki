ca4aee3656c7c7df488951b4b696641baa34bfaf
TED-exe <116170985+TED-exe@users.noreply.github.com>
2024-11-28 06:20:34 +0100

    BaseLobby

--- Assets/PlayerSpawner.cs
+++ Assets/PlayerSpawner.cs
@@ -1,25 +1,88 @@
-using System;
-using System.Collections.Generic;
-using System.Data;
-using Unity.Mathematics;
+﻿using Photon.Pun;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class PlayerSpawner : MonoBehaviour
+public class PlayerSpawner : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private List<GameObject> playerList;
+    private List<GameObject> globalPlayerList = new List<GameObject>();
 
     private void Start()
     {
-        
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            FirstPlayerSpawn();
+        }
     }
 
-    public void FirstaplayerSpawn()
+    public void FirstPlayerSpawn()
     {
-        var playerHodler = new GameObject($"PlayerHolder {playerList.Count + 1}");
-        float randomX = UnityEngine.Random.Range(0, 5);
-        float randomY = UnityEngine.Random.Range(0, 5);
+        if (playerPrefab == null)
+        {
+            Debug.LogError("Prefab gracza nie został przypisany!");
+            return;
+        }
 
-        Instantiate(playerPrefab, new Vector3(randomX, 1.5f, randomY), quaternion.identity);
+        float randomX = Random.Range(-5f, 5f);
+        float randomZ = Random.Range(-5f, 5f);
+        Vector3 spawnPosition = new Vector3(randomX, 1.5f, randomZ);
+
+        GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
+
+        if (player == null)
+        {
+            Debug.LogError("Nie udało się zespawnować gracza!");
+            return;
+        }
+
+        AddPlayerToGlobalList(player);
+    }
+
+    private void AddPlayerToGlobalList(GameObject player)
+    {
+        if (player == null)
+        {
+            Debug.LogError("Gracz jest null! Nie można dodać do listy.");
+            return;
+        }
+
+        PhotonView photonView = player.GetComponent<PhotonView>();
+        if (photonView == null)
+        {
+            Debug.LogError("PhotonView nie istnieje na prefabrykacie gracza!");
+            return;
+        }
+
+        globalPlayerList.Add(player);
+        photonView.RPC("RPC_AddPlayerToGlobalList", RpcTarget.OthersBuffered, photonView.ViewID);
+    }
+
+    [PunRPC]
+    private void RPC_AddPlayerToGlobalList(int viewID)
+    {
+        Debug.Log($"Otrzymano RPC_AddPlayerToGlobalList z ViewID: {viewID}");
+
+        PhotonView view = PhotonView.Find(viewID);
+        if (view == null)
+        {
+            Debug.LogError($"Nie znaleziono PhotonView o ViewID {viewID}!");
+            return;
+        }
+
+        GameObject player = view.gameObject;
+        globalPlayerList.Add(player);
+        Debug.Log($"Dodano gracza do globalnej listy: {player.name}");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("Zawartość globalnej listy graczy:");
+            foreach (var player in globalPlayerList)
+            {
+                Debug.Log(player?.name ?? "null");
+            }
+        }
     }
 }
